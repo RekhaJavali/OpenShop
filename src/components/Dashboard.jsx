dd4d4b65d08ece4c997755cf7b9771e087e7d949
REKHAE JAVALI <rekhajavali22@gmal.com>
2024-04-04 22:20:21 +0530

    dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { DashboardStatsGrid } from "./DashboardStatsGrid";
+import { Transaction } from "./Transaction";
+
 export  function Dashboard (){
  return(
-    <>
-    Dashboard page
-    <p><Link to="/products" >Go to Products</Link></p>
-    </>
+    <div className="flex flex-col gap-4 p-4">
+      <DashboardStatsGrid />
+     <Transaction />
+      <p>table</p>
+    
+    </div>
  )
 }
