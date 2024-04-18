3a9becdeac7c047a90e739f73fa36e9521720fec
REKHAE JAVALI <rekhajavali22@gmal.com>
2024-04-18 12:48:13 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import {BrowserRouter, Routes, Route,Link} from "react-router-dom";
 import { Dashboard }from "./components/Dashboard.jsx";
 import { Home } from "./components/Home.jsx";
 import Products from "./components/Products.jsx";
+import {Orders } from "./components/Orders.jsx";
+// import {Customers} from "./components/Customers.jsx";
 
 
 function App() {
@@ -18,7 +20,11 @@ function App() {
         <Route path="/" element={<Home/>}>
           <Route index element = {<Dashboard/>} />
           <Route path="products" element = {<Products/>}/>
-          {/* <Route path="dashboard" element = {<Dashboard/>} /> */}
+          {/* just to avoid error */}
+          <Route path="orders" element = {<Orders/>} />
+          <Route path="customers" element = {<h1>customer</h1>} />
+          <Route path="transactions" element = {<h1>Transactions</h1>} />
+          <Route path="messages" element = {<h1>Messages</h1>} />
         </Route>
       </Routes>
     </BrowserRouter>
