bebbf031d5f1ecc81a8634f0b833a1ed00063b4a
REKHAE JAVALI <rekhajavali22@gmal.com>
2024-04-18 13:05:24 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,10 @@ import { Header } from "./Header";
 export function Home(){
 return(
     <>
-    <div className="flex flex-row h-full w-screen md:w-full bg-neutral-100">
+    <div className="flex flex-row h-full w-screen md:w-full ">
         {/* changed from h-screen to h-full and w-screen to w-full */}
         <Sidebar className="fixed top-0 left-0 h-full overflow-hidden" />
-        <div className="flex-1 ">
+        <div className="flex-1 bg-neutral-100">
             <Header className= "sticky top-0 left-0 "/>
             <div>{<Outlet/>}</div>
         </div>
