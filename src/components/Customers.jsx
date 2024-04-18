cdb8ed2821d11847de6efb2f54914c66b2684d3b
REKHAE JAVALI <rekhajavali22@gmal.com>
2024-04-18 12:48:51 +0530

    customers

---
+++ src/components/Customers.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Customers(){
+    return(
+        <>
+        Customers page
+        <p className="underline"><Link to = "/">Go to Dashboard</Link></p>
+        </>
+
+    )
+}
