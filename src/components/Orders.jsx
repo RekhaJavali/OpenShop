840aa7d2350f7dc79befbfd6b5afd26496f3afd2
REKHAE JAVALI <rekhajavali22@gmal.com>
2024-04-19 20:46:20 +0530

    orders modified

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 
+import { Pagination } from "./Pagination";
+import { OrdersPagination } from "./OrdersPagination";
+// import { ORDERS } from "../lib/consts/orderTransaction" ;
+import {ORDER_ITEMS} from "../lib/consts/mockData";
+
+
+
 export  function Orders(){
+
+    const [posts, setPosts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postPerPage, SetPostPerPage] =useState(10);
+
+    // const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setPosts(ORDER_ITEMS);
+        // setLoading(false);
+    }, []
+    );
+
+    //get current post
+    const indexOfLastPost = currentPage * postPerPage;
+    const indexOfFirstPost = indexOfLastPost - postPerPage;
+    const curentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    //change the page
+    const handlePaginate=(pageNum)=>{
+        setCurrentPage(pageNum);
+    }
+
+    
+
     return(
         <>
-        Orders page
-        <p className="underline"><Link to = "/">Go to Dashboard</Link></p>
+       
+        <OrdersPagination posts = {curentPosts} />
+
+        <Pagination  postsPerPage = {postPerPage} totalPosts = {posts.length} paginate={handlePaginate}/>
         </>
 
     )
